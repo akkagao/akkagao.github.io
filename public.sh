cd4e0376469defa00fe6955abc0b9078dee50e3c
gaojianwen <gaojianwen@hualala.com>
2018-06-30 23:00:48 +0800

    2018-06-30:23时00分48秒 public dev

--- public.sh
+++ public.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
-
-hugo --theme=hugo-nuo --baseUrl="http://akkagao.github.io/"
+rm -rf public
+cd blog
+rm -rf public
+git checkout dev
+hugo --theme=hugo-nuo --baseUrl="https://akkagao.github.io/"
+git add .
+git commit -m "`date +%Y-%m-%d:%X` public dev"
+git push
+echo "blog.crazywolf.cc" > public/CNAME
+cp -r public ../
+git checkout master
+rm -rf *
+mv ../public/* ./
+git add .
+git commit -m " `date +%Y-%m-%d:%X` public master"
+git push
+git checkout dev
+cd ..
+rm -rf public
